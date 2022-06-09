16893f6640bb0e64d4a7931b64039b6535c8d055
AEBITco <107112221+AEBITco@users.noreply.github.com>
2022-06-09 19:29:51 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+namespace LEDRun{
+    //% blockId=LEDrunblock
+    //% block="PLOT at $x and $y"
+    //% x.min=0 x.max=4
+    //% y.min=0 y.max=4
+    export function plotAT(x: number, y: number): void{
+        led.plot(x,y)
+    }
+}
